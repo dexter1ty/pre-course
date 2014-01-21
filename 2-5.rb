b215a30da738b83d131b9c52c577d5a9e9540e0d
Clem <clement@engelberts.ch>
2014-01-21 08:18:28 +0100

    first exrcise

---
+++ 2-5.rb
@@ -0,0 +1,18 @@
+# Hours in a year.How many hours are in a year?
+
+puts 365 * 24
+
+
+# Minutes in a decade.How many minutes are in a decade?
+
+puts 10 * 365 * 24 * 60
+
+
+# Your age in seconds. How many seconds old are you?
+
+puts (32 * 365 - 47 + 7) * 24 * 3600 # (04/09/1982)
+
+
+# Our dear author’s age. If I am 1,160 million seconds old (which I am, though I was somewhere in the 800 millions when I started this book), how old am I?
+
+puts 1160000000 / ((365 + 8 / 36)* 24 * 3600) # Not sure if I correctly intergrated the bissextile years...
